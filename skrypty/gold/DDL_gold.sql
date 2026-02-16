abc82c9974e6dbe2bc5dc16d0973f1c02db81f12
Asia Sz <128564227+aszete@users.noreply.github.com>
2026-02-16 04:09:06 +0100

    Update DDL_gold.sql

--- skrypty/gold/DDL_gold.sql
+++ skrypty/gold/DDL_gold.sql
@@ -4,7 +4,7 @@ BEGIN
     CREATE TABLE gold.metadata (
         log_id INT IDENTITY(1,1) PRIMARY KEY NONCLUSTERED, -- PK is usually non-clustered in logs
         table_name VARCHAR(100),
-        source_layer VARCHAR(20) NOT NULL DEFAULT 'bronze',
+        source_layer VARCHAR(20) NOT NULL DEFAULT 'silver',
         load_start_time DATETIME2,
         load_end_time DATETIME2,
         duration_seconds AS DATEDIFF(SECOND, load_start_time, load_end_time),
