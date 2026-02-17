f78e9b2267b2ee00f299420dfd17c6055477ec7d
Asia Sz <128564227+aszete@users.noreply.github.com>
2026-02-17 21:19:37 +0100

    Update create_databse.sql

--- skrypty/create_databse.sql
+++ skrypty/create_databse.sql
@@ -33,21 +33,3 @@ CREATE SCHEMA silver;
 GO
 CREATE SCHEMA gold;
 GO
-
-==================================================================================
-# Why SIMPLE works for data warehouses:
-==================================================================================
-
-# Data warehouses are loaded from source systems through ETL/ELT processes
-# If something goes wrong, you can reload data from your source systems
-# You don't need point-in-time recovery since it's not handling live transactions
-# SIMPLE recovery prevents transaction logs from growing excessively during large data loads
-# It's more efficient for the bulk insert operations common in data warehouses
-
-# When you might use FULL for a data warehouse:
-
-# If your warehouse contains calculated/aggregated data that would be very expensive to recreate
-# If you have strict audit requirements for every change
-# If some data in the warehouse can't be recreated from sources
-
-# For a school project demonstrating a data warehouse, SIMPLE is definitely the standard choice.
