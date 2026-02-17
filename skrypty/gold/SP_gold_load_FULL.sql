b3c96035c8edffa32963019e20ff461484b82745
Asia Sz <128564227+aszete@users.noreply.github.com>
2026-02-18 00:08:06 +0100

    Create SP_gold_load_FULL.sql

---
+++ skrypty/gold/SP_gold_load_FULL.sql
@@ -0,0 +1,57 @@
+CREATE OR ALTER PROCEDURE gold.load_full
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    DECLARE @batch_start_time DATETIME2 = SYSDATETIME();
+    DECLARE @TotalDuration INT;
+    
+    PRINT '================================================';
+    PRINT '>>>          STARTING GOLD LAYER             <<<';
+    PRINT '================================================';
+
+    BEGIN TRY
+        -- PHASE 1: Independent Dimensions
+        PRINT 'Phase 1: Loading Date and Time...';
+        EXEC gold.load_dim_date;
+        EXEC gold.load_dim_time;
+
+        -- PHASE 2: Master Dimensions
+        PRINT 'Phase 2: Loading Customers and Products...';
+        EXEC gold.load_dim_customers;
+        EXEC gold.load_dim_products;
+
+        -- PHASE 4: Fact Tables
+        PRINT 'Phase 4: Loading Fact Tables...';
+		EXEC gold.load_fact_sessions;
+        EXEC gold.load_fact_sales;
+        EXEC gold.load_fact_web_analytics;
+        EXEC gold.load_fact_returns;
+
+        SET @TotalDuration = DATEDIFF(SECOND, @batch_start_time, SYSDATETIME());
+
+        PRINT '================================================';
+        PRINT '>>>         LOADING GOLD SUCCESSFUL          <<<';
+        PRINT 'Duration: ' + CAST(@TotalDuration AS VARCHAR(10)) + ' seconds';
+        PRINT '================================================';
+
+    END TRY
+    BEGIN CATCH
+        -- Capture the error message immediately
+        DECLARE @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE();
+        
+        PRINT '!!! ERROR !!!';
+        PRINT @ErrorMessage;
+        
+        -- Log failure to metadata
+        EXEC gold.log_metadata 
+            @table_name = 'ORCHESTRATOR_FAILURE', 
+            @start_time = @batch_start_time, 
+            @ins = 0, 
+            @status = 'Error', 
+            @error = @ErrorMessage;
+
+        -- Rethrow to notify external tools
+        THROW 50000, @ErrorMessage, 1; 
+    END CATCH
+END;
